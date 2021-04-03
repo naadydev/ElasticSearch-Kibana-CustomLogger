303e07c8d00be48926b5a476c7e85cb2cdd4f846
NADY <naadydev@outlook.com>
2021-04-03 12:30:25 +0200

    customL

--- ElasticSearch.Kibana-Logger/ElasticSearch.Helper/ActionLog.cs
+++ ElasticSearch.Kibana-Logger/ElasticSearch.Helper/ActionLog.cs
@@ -5,11 +5,6 @@
         public string UserId { get; set; }
         public string UserEmail { get; set; }
         public string Message { get; set; }
-        // 3 points 
-        // Put the latest emplyeer first 
-        // More than 8 years leading teams 
-        // Reasearch center Team Lead (how many people , and tech)
-        // 
     }
 
     public enum SysAction
